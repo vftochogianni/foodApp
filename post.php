2d9aebc58e0117acad9080f0a726c4394c046f17
vftochogianni <vftochogianni@gmail.com>
2016-07-09 14:43:05 +0300

    Update post.php

--- post.php
+++ post.php
@@ -11,6 +11,7 @@
         {
             case 'image/gif': return '.gif';
             case 'image/jpeg': return '.jpg';
+            case 'image/jpg': return '.jpg';
             case 'image/png': return '.png';
             default: return false;
         }
